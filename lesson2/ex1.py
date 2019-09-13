c2e05235a326a680f713b423ca2c420cd46931f0
iizakharov <45385589+iizakharov@users.noreply.github.com>
2019-09-13 10:56:05 +0300

    fix1

--- lesson2/ex1.py
+++ lesson2/ex1.py
@@ -17,36 +17,51 @@
 
 import csv
 import re
-from ast import literal_eval as le
 
 
-# def get_data(**kwargs):
-arr = []
-with open('info_1.txt') as f_n:
-    f_n_reader = csv.reader(f_n)
-    for row in f_n_reader:
-        myString = ';'.join(row)
-        new_row = re.sub(r'\s{2,}', ' ', myString)
-        print(new_row)
-        # arr.append(new_row)
-# with open('info_1.csv', 'w') as f_n:
-#     f_n_writer = csv.writer(f_n)
-#     for row_ in arr:
-#         f_n_writer.writerow(row_)
+os_prod_list = []
+os_name_list = []
+os_code_list = []
+os_type_list = []
+main_data = [{
+    'Изготовитель системы': os_prod_list,
+    'Название ОС': os_name_list,
+    'Код продукта': os_code_list,
+    'Тип системы': os_type_list,
+}]
 
 
-# print(arr)
-#     return arr
-#
-# print(get_data())
+def get_data(file):
+    with open(file) as f_n:
+        f_n_reader = csv.reader(f_n)
+        for row in f_n_reader:
+            myString = ''.join(row)
+            new_row = re.sub(r'\s{2,}', ' ', myString)
+            if 'Изготовитель системы' in new_row:
+                list_row = new_row.split(':')
+                os_prod_list.append(list_row[1])
+            elif 'Название ОС' in new_row:
+                list_row = new_row.split(':')
+                os_name_list.append(list_row[1])
+            elif 'Код продукта' in new_row:
+                list_row = new_row.split(':')
+                os_code_list.append(list_row[1])
+            elif 'Тип системы' in new_row:
+                list_row = new_row.split(':')
+                os_type_list.append(list_row[1])
+
+get_data('info_1.txt')
+get_data('info_2.txt')
+get_data('info_3.txt')
+
+# print(list(main_data.keys()))
 
 
+def write_to_csv(file_csv, data):
+    with open(file_csv, 'w', encoding='utf-8') as f_n:
+        f_n_writer = csv.DictWriter(f_n, fieldnames=list(data[0].keys()))
+        f_n_writer.writeheader()
+        for d in data:
+            f_n_writer.writerow(d)
 
-
-# for i in arr:
-#     while "  " in arr[i]:
-#         row = arr.replace("  ", " ")
-# print(arr)
-
-# print(re.sub('\s+', ' ', arr))
-# print(re.sub(r'\s', '', arr))
+write_to_csv('file_1.csv', main_data)
--- lesson2/test.py
+++ lesson2/test.py
@@ -11,16 +11,23 @@
 # print(type(myString))
 # print(myString)
 
-import re
+# import re
+#
+# with open('info_1.txt') as f_n:
+#     arr = []
+#     for row in f_n:
+#         # myString = ';'.join(row)
+#         # new_row = re.sub(r'\s{2,}', ' ', myString)
+#         # print(new_row)
+#         new_row = re.sub(r'\s{2,}', ' ', row)
+#         # print(new_row)
+#         arr.append(new_row)
+#     print(arr)
+#     # myString = ';'.join(arr)
 
-with open('info_1.txt') as f_n:
-    arr = []
-    for row in f_n:
-        # myString = ';'.join(row)
-        # new_row = re.sub(r'\s{2,}', ' ', myString)
-        # print(new_row)
-        new_row = re.sub(r'\s{2,}', ' ', row)
-        # print(new_row)
-        arr.append(new_row)
-    print(arr)
-    # myString = ';'.join(arr)
+a = 'Название ОС: Microsoft Windows 7 Профессиональная'
+b = a.split(':')  # b = ['python', ' django', ' template   tags']
+print(b)
+print(b[1])
+c = dict(b)
+print(c)
